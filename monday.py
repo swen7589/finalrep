ecba668baa50543d3566a8ce0269b06c738e4d3f
swen7589 <89879508+swen7589@users.noreply.github.com>
2021-12-07 18:22:17 -0800

    learning

--- monday.py
+++ monday.py
@@ -1,9 +1,14 @@
 # pip install flask for this to work
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return 'hello <b>world</b>'
+
+@app.route('/users')
+def users():
+    return render_template('users.html', username='Mike')
+
 
 app.run()
